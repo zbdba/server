551edef8adcc119e9d3c3c4ba06d9bc385bbae23
Eugene Kosov <eugene.kosov@mariadb.com>
2020-05-19 14:19:20 +0300

    MDEV-21212 buf_page_get_gen -> buf_pool->stat.n_page_gets++ is a cpu waste (0.5-1%)
    This patch is inspired by wg21.link/P0261
    
    Fix by introducing yet another distributed counter. This one uses
    C++11 thread_local specifier to achive a perfect scalability.
    
    In the following benchmark ShardedAtomic is a counter
    with 64 shards we use now.
    
    Single thread mode is faster because of a weak atomical semantics of a new
    counter. Strong semantics is not needed because only one writer exists for
    each counter.
    
    Running ./counter-benchmark
    Run on (128 X 200 MHz CPU s)
    CPU Caches:
      L1 Data 64 KiB (x128)
      L1 Instruction 64 KiB (x128)
      L2 Unified 512 KiB (x128)
      L3 Unified 65536 KiB (x4)
    Load Average: 0.00, 0.13, 0.22
    ------------------------------------------------------------------------
    Benchmark                              Time             CPU   Iterations
    ------------------------------------------------------------------------
    BM_SimpleAtomic/threads:1           6.16 ns         6.16 ns    113705842
    BM_SimpleAtomic/threads:2           8.17 ns         16.3 ns     42821142
    BM_SimpleAtomic/threads:4           21.6 ns         86.5 ns     12652320
    BM_SimpleAtomic/threads:8           33.3 ns          266 ns      2148016
    BM_SimpleAtomic/threads:16           133 ns         2130 ns       487520
    BM_SimpleAtomic/threads:32           124 ns         3952 ns       217344
    BM_SimpleAtomic/threads:64           157 ns        10026 ns        64000
    BM_SimpleAtomic/threads:128         94.6 ns        12102 ns       128000
    BM_SimpleAtomic/threads:256         60.4 ns        15446 ns       256000
    BM_SimpleAtomic/threads:512         26.8 ns        13714 ns       512000
    BM_SimpleAtomic/threads:1024        14.5 ns        14889 ns       102400
    BM_SimpleAtomic/threads:2048        5.65 ns        11566 ns       204800
    BM_ShardedAtomic/threads:1          6.16 ns         6.16 ns    113679824
    BM_ShardedAtomic/threads:2          3.08 ns         6.16 ns    113721534
    BM_ShardedAtomic/threads:4          1.54 ns         6.16 ns    113711620
    BM_ShardedAtomic/threads:8         0.769 ns         6.16 ns    113712616
    BM_ShardedAtomic/threads:16        0.385 ns         6.16 ns    113628336
    BM_ShardedAtomic/threads:32        0.192 ns         6.16 ns    113594432
    BM_ShardedAtomic/threads:64        0.228 ns         14.6 ns     52447616
    BM_ShardedAtomic/threads:128       0.195 ns         25.0 ns     30016768
    BM_ShardedAtomic/threads:256       0.179 ns         45.8 ns     25600000
    BM_ShardedAtomic/threads:512       0.122 ns         62.6 ns     58018304
    BM_ShardedAtomic/threads:1024      0.073 ns         74.9 ns     10240000
    BM_ShardedAtomic/threads:2048      0.054 ns          110 ns     20480000
    BM_TlsCounter/threads:1             2.71 ns         2.71 ns    259471815
    BM_TlsCounter/threads:2             1.35 ns         2.69 ns    255314508
    BM_TlsCounter/threads:4            0.674 ns         2.70 ns    259838580
    BM_TlsCounter/threads:8            0.337 ns         2.69 ns    259870616
    BM_TlsCounter/threads:16           0.169 ns         2.71 ns    257862656
    BM_TlsCounter/threads:32           0.084 ns         2.70 ns    259839840
    BM_TlsCounter/threads:64           0.043 ns         2.72 ns    257231104
    BM_TlsCounter/threads:128          0.021 ns         2.70 ns    258558848
    BM_TlsCounter/threads:256          0.011 ns         2.70 ns    257301248
    BM_TlsCounter/threads:512          0.005 ns         2.71 ns    257305088
    BM_TlsCounter/threads:1024         0.003 ns         2.72 ns    246151168
    BM_TlsCounter/threads:2048         0.001 ns         2.74 ns    235972608
    
    Several counters were translated to a new one because I suppose
    they could be slow.

--- storage/innobase/btr/btr0sea.cc
+++ storage/innobase/btr/btr0sea.cc
@@ -1126,7 +1126,7 @@ got_no_latch:
 		}
 		mtr->memo_push(block, fix_type);
 
-		buf_pool.stat.n_page_gets++;
+		COUNTER(N_PAGE_GETS)++;
 
 		rw_lock_s_unlock(&part->latch);
 
@@ -1216,7 +1216,7 @@ fail_and_release_page:
 #endif
 	/* Increment the page get statistics though we did not really
 	fix the page: for user info only */
-	++buf_pool.stat.n_page_gets;
+	++COUNTER(N_PAGE_GETS);
 
 	if (!ahi_latch) {
 		buf_page_make_young_if_needed(&block->page);
